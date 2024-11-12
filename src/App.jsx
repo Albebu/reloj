dd936b231c12acdbbb4b1816b9bf7b58effb0a33
Alex Bellosta Buiza <abellostabuiza@gmail.com>
2024-11-12 09:26:34 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Clock from './components/Clock'
+
+export default function App() {
+  return (
+    <div className='flex h-screen items-center justify-center text-6xl'>
+      <Clock></Clock>
+    </div>
+  )
+}
